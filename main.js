1c7ef3c26c9dcaa33b31b6e72d86044c01eb4b45
ssebastianoo <sebastianogirotto05@gmail.com>
2022-06-19 03:44:05 +0200

    init

---
+++ main.js
@@ -0,0 +1,21 @@
+const { Telegraf } = require('telegraf');
+const config = require('./config');
+
+const bot = new Telegraf(config.token);
+bot.start((ctx) => ctx.reply("just tag me in a chat and i'll send the hihiha\n\nby @ssebastianoo"));
+
+bot.on('inline_query', (ctx) => {
+    const result = [{
+        type: 'audio',
+        id: 'hihihiha',
+        audio_url: 'https://morbinguniversity-it.vercel.app/hihihiha.mp3',
+        title: 'HIHIHIHA',
+    }]
+    ctx.answerInlineQuery(result)
+  })
+
+bot.launch();
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
